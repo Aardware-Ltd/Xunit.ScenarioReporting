7c9bb63c5101efaf17f8ba36bda080f980e35e1b
MJ <cosaswm@gmail.com>
2017-09-12 12:18:14 +0200

    Updates Detail so it can hold child details

--- Xunit.ScenarioReporting/Results/Detail.cs
+++ Xunit.ScenarioReporting/Results/Detail.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Xunit.ScenarioReporting.Results
 {
@@ -8,6 +9,9 @@ namespace Xunit.ScenarioReporting.Results
         public object Value { get; }
         public string Format { get; }
         public Func<object, string> Formatter { get; }
+        public IReadOnlyList<Detail> Children => _children;
+
+        private List<Detail> _children; 
 
         public Detail(string name, object value, string format = null, Func<object, string> formatter = null)
         {
@@ -15,6 +19,12 @@ namespace Xunit.ScenarioReporting.Results
             Value = value;
             Format = format;
             Formatter = formatter;
+            _children = new List<Detail>();
+        }
+
+        public Detail(Detail parent, string name, object value, string format = null, Func<object, string> formatter = null) : this(name, value, format, formatter)
+        {
+            parent._children.Add(this);
         }
     }
 }
