2242c1fb6f56317d421d1a99af966c8fae408dc4
James Geall <jageall@gmail.com>
2017-09-29 16:54:39 +0300

    Add example for When failing

---
+++ Examples/FailingWhen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.ScenarioReporting;
+
+namespace Examples
+{
+    public class FailingWhen
+    {
+        [Fact]
+        public Task<ScenarioRunResult> ShouldFail()
+        {
+            return new TestRunner().Run(def => def.Given().When(new object()).Then());
+        }
+
+        class TestRunner : ReflectionBasedScenarioRunner<object, object, object>
+        {
+            protected override Task Given(IReadOnlyList<object> givens)
+            {
+                return Task.CompletedTask;
+            }
+
+            protected override Task When(object when)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override Task<IReadOnlyList<object>> ActualResults()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
